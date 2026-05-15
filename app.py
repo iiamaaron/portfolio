219a6c609088cd0500679ac2e575c3e67c6c3663
Aaron Tetteh <aarontetteh2004@gmail.com>
2026-05-15 15:58:25 +0000

    Debug SendGrid error

--- app.py
+++ app.py
@@ -87,6 +87,7 @@ def contact():
         sg.send(sg_mail)
     except Exception as e:
         print(f"Mail error: {e}")
+        return jsonify({'mail_error': str(e)}), 200
 
     return jsonify({'success': 'Message received'}), 200
 
